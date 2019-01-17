dbe3b06b6a345734e5bc3432d24b5e9e4132aaef
Meelis Valgeväli <meelis@adm.ee>
2019-01-17 09:21:58 +0200

    fixnum to string

--- src/base/build/modules/build/manifests/cron/supervisor.pp
+++ src/base/build/modules/build/manifests/cron/supervisor.pp
@@ -2,6 +2,6 @@ class build::cron::supervisor {
   file { '/etc/supervisor/conf.d/cron.conf':
     ensure => present,
     source => 'puppet:///modules/build/etc/supervisor/conf.d/cron.conf',
-    mode => 644
+    mode => "644"
   }
 }
