5c48860512357eb01df1dd48658cdc912a77e354
Meelis Valgeväli <meelis@adm.ee>
2020-02-04 17:38:10 +0200

    better cleanup.

--- src/base/clean/modules/clean/manifests/init.pp
+++ src/base/clean/modules/clean/manifests/init.pp
@@ -1,5 +1,18 @@
 class clean {
-  bash_exec { 'apt-get clean': }
+
+  bash_exec { 'apt-get --purge -y remove python3.7 python3.7-minimal git-man geoip-database': }
+
+  bash_exec { 'apt-get remove -y build-essential autoconf automake':
+    require => Bash_exec[ 'apt-get --purge -y remove python3.7 python3.7-minimal git-man geoip-database' ]
+  }
+
+  bash_exec { 'apt -y autoremove':
+    require => Bash_exec[ 'apt-get remove -y build-essential autoconf automake' ]
+  }
+
+  bash_exec { 'apt-get clean':
+    require => Bash_exec[ 'apt -y autoremove' ]
+  }
 
   bash_exec { 'rm -rf /tmp/*': }
 
